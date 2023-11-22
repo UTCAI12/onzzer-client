8a1c863658e67fe10394958a6bc8d7c7a17f315e
Syrows <clement.gho@gmail.com>
2023-11-22 17:23:18 +0100

    Initializing project.

---
+++ src/main/java/fr/utc/onzzer/onzzerclient/main/RegisterViewController.java
@@ -0,0 +1,28 @@
+package fr.utc.onzzer.onzzerclient.main;
+
+import fr.utc.onzzer.onzzerclient.MainClient;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterViewController {
+
+    @FXML
+    private Label connectionLabel;
+
+    @FXML
+    private void onConnectionLabelClick() throws IOException {
+
+        Stage stage = MainClient.getStage();
+        Scene current = stage.getScene();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainClient.class.getResource("/fxml/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), current.getWidth(), current.getHeight());
+
+        stage.setScene(scene);
+    }
+}
