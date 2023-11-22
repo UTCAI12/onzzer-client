088285dc7e90ef0b3a13d6c2893ab74aa73446df
abdel <dorra.abdelkefi@etu.utc.fr>
2023-11-22 19:16:28 +0100

    On Registration Button Click

--- src/main/java/fr/utc/onzzer/onzzerclient/main/RegisterViewController.java
+++ src/main/java/fr/utc/onzzer/onzzerclient/main/RegisterViewController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
@@ -13,7 +14,8 @@ public class RegisterViewController {
 
     @FXML
     private Label connectionLabel;
-
+    @FXML
+    private Button registrationButton;
     @FXML
     private void onConnectionLabelClick() throws IOException {
 
@@ -21,6 +23,18 @@ public class RegisterViewController {
         Scene current = stage.getScene();
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainClient.class.getResource("/fxml/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), current.getWidth(), current.getHeight());
+
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void onRegistrationButtonClick() throws IOException {
+
+        Stage stage = MainClient.getStage();
+        Scene current = stage.getScene();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainClient.class.getResource("/fxml/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), current.getWidth(), current.getHeight());
 
         stage.setScene(scene);
