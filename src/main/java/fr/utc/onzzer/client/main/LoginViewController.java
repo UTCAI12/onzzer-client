5ee7e848bcaf1c0bf5824ff796b5645c6d8cd9ee
ilianAZZ <ilian572@gmail.com>
2023-11-23 22:47:30 +0100

    on "login" button click, notifies server of new client

--- src/main/java/fr/utc/onzzer/client/main/LoginViewController.java
+++ src/main/java/fr/utc/onzzer/client/main/LoginViewController.java
@@ -1,14 +1,22 @@
 package fr.utc.onzzer.client.main;
 
 import fr.utc.onzzer.client.MainClient;
+import fr.utc.onzzer.client.common.communication.ClientCommunicationController;
+import fr.utc.onzzer.client.common.services.ComMainServices;
+import fr.utc.onzzer.common.dataclass.ClientModel;
+import fr.utc.onzzer.common.dataclass.User;
+import fr.utc.onzzer.common.dataclass.UserLite;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class LoginViewController {
 
@@ -17,6 +25,15 @@ public class LoginViewController {
 
     @FXML
     private Label registerLabel;
+
+    @FXML
+    private TextField txtServerIp;
+
+    @FXML
+    private TextField txtServerPort;
+
+    @FXML
+    private TextField txtUserPseudo;
 
     @FXML
     private void onRegisterLabelClick() throws IOException {
@@ -32,6 +49,16 @@ public class LoginViewController {
 
     @FXML
     private void onLoginButtonClick() throws IOException {
+
+        // TODO temporairement pour voir si ca marche bien. A terme faire une méthode à côté
+        User user = new User(UUID.randomUUID(), txtUserPseudo.getText(), "mail", "mdp");
+        UserLite userLite = new UserLite(user.getId(), user.getUsername());
+        ClientModel clientModel = new ClientModel(user);
+        ComMainServices comMainServices = new ClientCommunicationController(
+                this.txtServerIp.getText(), Integer.parseInt(this.txtServerPort.getText()), clientModel);
+        comMainServices.connect(userLite, new ArrayList<>());
+        // TODO Fin
+
 
         Stage stage = MainClient.getStage();
         Scene current = stage.getScene();
