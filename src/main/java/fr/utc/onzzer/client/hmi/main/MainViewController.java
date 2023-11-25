d32c24909189ac26aee01bb36e624fc804868e72
Matthieu SOLAN <matthieu.solan@etu.utc.fr>
2023-11-25 18:44:12 +0100

    usersList

--- src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
@@ -1,11 +1,33 @@
 package fr.utc.onzzer.client.hmi.main;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import fr.utc.onzzer.common.dataclass.User;
+import fr.utc.onzzer.common.dataclass.ClientModel;
+
 public class MainViewController {
 
-    /**
-     * This method is automatically called by JavaFX on controller instantiation
-     */
+    @FXML
+    private ListView<String> usersList;
+
+    private final ClientModel model;
+
+    // If the model triggers an "NEW_USER" event, call the "updateAllUsers" method
+    this.model.addListener(newUser -> this.UpdateUsersList(), UserLite.class, ModelUpdateTypes.NEW_USER);
+
+    // If the model triggers an "DELETE_USER" event, call the "updateAllUsers" method
+    this.model.addListener(newUser -> this.UpdateUsersList(), UserLite.class, ModelUpdateTypes.DELETE_USER);
+
+
+    private void UpdateUsersList()
+    {
+        this.usersList.getItems().clear();
+        this.model.others.forEach(user -> this.usersList.getItems().add(user.getName()));
+
+    }
+
     public void initialize() {
         System.out.println("coucou");
+
     }
 }
