91a7af14346e6dc2c2676f9b0b3fc88a66b2637d
Matthieu SOLAN <matthieu.solan@etu.utc.fr>
2023-11-28 23:58:15 +0100

    update of login view and user connected on main view

--- src/main/java/fr/utc/onzzer/client/main/MainViewController.java
+++ src/main/java/fr/utc/onzzer/client/main/MainViewController.java
@@ -1,32 +1,72 @@
 package fr.utc.onzzer.client.main;
 import javafx.fxml.FXML;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 import fr.utc.onzzer.common.dataclass.User;
 import fr.utc.onzzer.common.dataclass.ClientModel;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class MainViewController {
+
 
     @FXML
     private ListView<String> usersList;
 
-    private final ClientModel model;
+
+    @FXML
+    public TextField searchField;
+
+    private ClientModel model;
+
+    private ObservableList<String> usernamesList = FXCollections.observableArrayList("Ilian", "Clement", "Matthieu");
 
     // If the model triggers an "NEW_USER" event, call the "updateAllUsers" method
-    this.model.addListener(newUser -> this.UpdateUsersList(), UserLite.class, ModelUpdateTypes.NEW_USER);
+    //this.model.addListener(newUser -> this.UpdateUsersList(newUser.toString,true), UserLite.class, ModelUpdateTypes.NEW_USER);
 
     // If the model triggers an "DELETE_USER" event, call the "updateAllUsers" method
-    this.model.addListener(newUser -> this.UpdateUsersList(), UserLite.class, ModelUpdateTypes.DELETE_USER);
+    //this.model.addListener(newUser -> this.UpdateUsersList(newUser.toString,false), UserLite.class, ModelUpdateTypes.DELETE_USER);
 
 
-    private void UpdateUsersList()
+    private void UpdateUsersList(String user,boolean IsAddUser)
     {
         this.usersList.getItems().clear();
-        this.model.others.forEach(user -> this.usersList.getItems().add(user.getName()));
+        //this.model.others.forEach(user -> this.usersList.getItems().add(user.getName()));
+
+        if(IsAddUser)
+        {
+            usernamesList.add(user);
+        }
+        else
+        {
+            usernamesList.remove(user);
+        }
+
+        for (String element : usernamesList) {
+            this.usersList.getItems().add(element);
+        }
+    }
+
+    @FXML
+    private void handleSearch()
+    {
+        ObservableList<String> filteredList = usernamesList.filtered(item -> item.toLowerCase().contains(searchField.getText().toLowerCase()));
+
+        this.usersList.getItems().clear();
+        // Mettre à jour la liste affichée dans la vue
+        for (String element : filteredList) {
+            this.usersList.getItems().add(element);
+        }
 
     }
 
+
     public void initialize() {
         System.out.println("coucou");
+        for (String element : usernamesList) {
+            this.usersList.getItems().add(element);
+        }
 
     }
 }
