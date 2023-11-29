166ea8f8a7cb91efa8405b8d296cda9b0e58ff00
Matthew-prog <matthewjoy53@gmail.com>
2023-11-29 12:57:25 +0100

    Fix issues

--- src/main/java/fr/utc/onzzer/client/data/DataUserServices.java
+++ src/main/java/fr/utc/onzzer/client/data/DataUserServices.java
@@ -2,14 +2,21 @@ package fr.utc.onzzer.client.data;
 
 import fr.utc.onzzer.common.dataclass.User;
 import fr.utc.onzzer.common.dataclass.UserLite;
+import fr.utc.onzzer.common.services.Service;
 
-public interface DataUserServices {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface DataUserServices extends Service {
     User importProfile(String filePath) throws Exception;
     void createProfile(User newClient) throws Exception;
     boolean checkCredentials(String user, String pw) throws Exception;
     void exportProfile(User user, String filePath) throws Exception;
     User getUser() throws Exception;
+    public List<UserLite> getConnectedUsers();
     void addUser(UserLite userLite) throws Exception;
+    void setConnectedUsers(final List<UserLite> connectedUsers);
     void deleteUser(UserLite userLite) throws Exception;
     void updateUser(User user) throws Exception;
     void deleteAllUsers();
