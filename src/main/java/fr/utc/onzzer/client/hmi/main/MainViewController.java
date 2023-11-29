f96a73920bb74b3d14ccac5539160df923cd0ee7
Matthieu SOLAN <matthieu.solan@etu.utc.fr>
2023-11-29 22:42:20 +0100

    add listener and getUsersConnected method to main controller

--- src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
@@ -1,33 +1,36 @@
 package fr.utc.onzzer.client.hmi.main;
 
+import fr.utc.onzzer.client.data.DataServicesProvider;
+import fr.utc.onzzer.client.data.DataUserServices;
 import fr.utc.onzzer.client.hmi.GlobalController;
-import javafx.fxml.FXML;
-import javafx.collections.ObservableList;
-import javafx.scene.control.ListView;
-import fr.utc.onzzer.common.dataclass.User;
 import fr.utc.onzzer.common.dataclass.ClientModel;
+import fr.utc.onzzer.common.dataclass.ModelUpdateTypes;
+import fr.utc.onzzer.common.dataclass.UserLite;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainViewController {
 
+    private List<UserLite> usersConnected;
     private final GlobalController controller;
+    @FXML
+    public TextField searchField;
+
+    @FXML
+    private ListView<String> usersList;
+    private ClientModel model;
+    private final ObservableList<String> usernamesList = FXCollections.observableArrayList("Ilian", "Clement", "Matthieu");
 
     public MainViewController(GlobalController controller) {
         this.controller = controller;
     }
-
-    @FXML
-    private ListView<String> usersList;
-
-
-    @FXML
-    public TextField searchField;
-
-    private ClientModel model;
-
-    private ObservableList<String> usernamesList = FXCollections.observableArrayList("Ilian", "Clement", "Matthieu");
 
     // If the model triggers an "NEW_USER" event, call the "updateAllUsers" method
     //this.model.addListener(newUser -> this.UpdateUsersList(newUser.toString,true), UserLite.class, ModelUpdateTypes.NEW_USER);
@@ -35,18 +38,13 @@ public class MainViewController {
     // If the model triggers an "DELETE_USER" event, call the "updateAllUsers" method
     //this.model.addListener(newUser -> this.UpdateUsersList(newUser.toString,false), UserLite.class, ModelUpdateTypes.DELETE_USER);
 
-
-    private void UpdateUsersList(String user,boolean IsAddUser)
-    {
+    private void UpdateUsersList(String user, boolean IsAddUser) {
         this.usersList.getItems().clear();
         //this.model.others.forEach(user -> this.usersList.getItems().add(user.getName()));
 
-        if(IsAddUser)
-        {
+        if (IsAddUser) {
             usernamesList.add(user);
-        }
-        else
-        {
+        } else {
             usernamesList.remove(user);
         }
 
@@ -56,8 +54,7 @@ public class MainViewController {
     }
 
     @FXML
-    private void handleSearch()
-    {
+    private void handleSearch() {
         ObservableList<String> filteredList = usernamesList.filtered(item -> item.toLowerCase().contains(searchField.getText().toLowerCase()));
 
         this.usersList.getItems().clear();
@@ -70,10 +67,39 @@ public class MainViewController {
 
 
     public void initialize() {
-        System.out.println("coucou");
-        for (String element : usernamesList) {
-            this.usersList.getItems().add(element);
-        }
+
+        //usersConnected = dataUserServices.getConnectedUsers();
+/*        System.out.println("coucou");
+        for (UserLite element : usersConnected) {
+            this.usersList.getItems().add(element.getUsername());
+        }*/
+
+        DataServicesProvider dataServicesProvider = this.controller.getDataServicesProvider();
+        DataUserServices dataUserServices = dataServicesProvider.getDataUserServices();
+
+        // Adding a listener to get the new user connected
+       dataUserServices.addListener(user -> {
+            Platform.runLater(() -> {
+                try {
+                    this.usersList.getItems().add(user.getUsername());
+
+                } catch (Exception exception) {
+                    throw new RuntimeException(exception);
+                }
+            });
+        }, UserLite.class, ModelUpdateTypes.NEW_USER);
+
+        // Adding a listener to get the new user connected
+        dataUserServices.addListener(user -> {
+            Platform.runLater(() -> {
+                try {
+                    this.usersList.getItems().remove(user.getUsername());
+                } catch (Exception exception) {
+                    throw new RuntimeException(exception);
+                }
+            });
+        }, UserLite.class, ModelUpdateTypes.DELETE_USER);
+
 
     }
 }
