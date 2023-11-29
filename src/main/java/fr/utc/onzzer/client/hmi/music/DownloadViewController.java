ae7a84955e3d5fe9d13c168f0ebaa1fd4902224f
MyriemKh <myriem.khal@gmail.com>
2023-11-29 15:46:34 +0100

    fxml file and controller for track download

---
+++ src/main/java/fr/utc/onzzer/client/hmi/music/DownloadViewController.java
@@ -0,0 +1,26 @@
+package fr.utc.onzzer.client.hmi.music;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class DownloadViewController {
+
+    @FXML
+    private TextField txtTitle;
+
+    @FXML
+    private TextField txtAuthor;
+
+    @FXML
+    private TextField txtAlbum;
+
+    @FXML
+    private Button btnDownload;
+
+    @FXML
+    public void onClickDownload(ActionEvent actionEvent) {
+
+    }
+}
