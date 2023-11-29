f227e180260fe9b45eb28714f793e3d8b0e15aa4
César Dieudonné <cesardieudonne7@gmail.com>
2023-11-29 18:50:30 +0100

    Music list

--- src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
@@ -1,11 +1,36 @@
 package fr.utc.onzzer.client.hmi.main;
 
-public class MainViewController {
+import fr.utc.onzzer.common.dataclass.Track;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
-    /**
-     * This method is automatically called by JavaFX on controller instantiation
-     */
+import java.util.UUID;
+
+public class MainViewController {
+    @FXML
+    private TableView<Track> tableau;
+
+    @FXML
+    private TableColumn<Track, String> colonneTitre;
+
+    @FXML
+    private TableColumn<Track, String> colonneAuteur;
+
+    // Initialisation
     public void initialize() {
-        System.out.println("coucou");
+        colonneTitre.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colonneAuteur.setCellValueFactory(new PropertyValueFactory<>("author"));
+
+        ObservableList<Track> tracks = FXCollections.observableArrayList();
+        tracks.add(new Track(UUID.randomUUID(), UUID.randomUUID(), "Titre 1", "Auteur 1"));
+        tracks.add(new Track(UUID.randomUUID(), UUID.randomUUID(), "Titre 2", "Auteur 2"));
+
+        tableau.setItems(tracks);
     }
+
+    // Autres méthodes et logique du contrôleur
 }
