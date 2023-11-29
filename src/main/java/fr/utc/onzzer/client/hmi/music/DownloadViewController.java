8178c4e8124376ca2bcb1d18d49c0728ef0dfc1d
MyriemKh <myriem.khal@gmail.com>
2023-11-29 17:22:04 +0100

    add globalController

--- src/main/java/fr/utc/onzzer/client/hmi/music/DownloadViewController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/music/DownloadViewController.java
@@ -1,11 +1,18 @@
 package fr.utc.onzzer.client.hmi.music;
 
+import fr.utc.onzzer.client.hmi.GlobalController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class DownloadViewController {
+public class DownloadViewController{
+
+    private final GlobalController controller;
+
+    public DownloadViewController(GlobalController controller) {
+        this.controller = controller;
+    }
 
     @FXML
     private TextField txtTitle;
