0ad6924c0cf3e7c5d512cf591880f2ef89aeb466
ilianAZZ <ilian572@gmail.com>
2023-11-30 21:52:18 +0100

    Changing import for new service impl sub package

--- src/main/java/fr/utc/onzzer/client/hmi/GlobalController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/GlobalController.java
@@ -3,7 +3,7 @@ package fr.utc.onzzer.client.hmi;
 import fr.utc.onzzer.client.communication.ComServicesProvider;
 import fr.utc.onzzer.client.data.DataServicesProvider;
 import fr.utc.onzzer.client.hmi.music.services.ViewMusicServices;
-import fr.utc.onzzer.client.hmi.music.services.ViewMusicServicesImpl;
+import fr.utc.onzzer.client.hmi.music.services.impl.ViewMusicServicesImpl;
 
 public class GlobalController {
     private DataServicesProvider dataServicesProvider = new DataServicesProvider();
