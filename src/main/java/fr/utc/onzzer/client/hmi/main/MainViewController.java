41b0aad0c510c6e62e3f6bf81771366e1e9170a9
Syrows <clement.gho@gmail.com>
2023-12-02 00:33:08 +0100

    Fixing list initialization with connected users. Fixing search. Refactoring code.

--- src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/main/MainViewController.java
@@ -3,103 +3,99 @@ package fr.utc.onzzer.client.hmi.main;
 import fr.utc.onzzer.client.data.DataServicesProvider;
 import fr.utc.onzzer.client.data.DataUserServices;
 import fr.utc.onzzer.client.hmi.GlobalController;
-import fr.utc.onzzer.common.dataclass.ClientModel;
 import fr.utc.onzzer.common.dataclass.ModelUpdateTypes;
+import fr.utc.onzzer.common.dataclass.TrackLite;
 import fr.utc.onzzer.common.dataclass.UserLite;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public class MainViewController {
 
-    private List<UserLite> usersConnected;
     private final GlobalController controller;
+
     @FXML
-    public TextField searchField;
+    private TextField searchField;
 
     @FXML
     private ListView<String> usersList;
-    private ClientModel model;
-    private final ObservableList<String> usernamesList = FXCollections.observableArrayList("Ilian", "Clement", "Matthieu");
 
     public MainViewController(GlobalController controller) {
         this.controller = controller;
     }
 
-    // If the model triggers an "NEW_USER" event, call the "updateAllUsers" method
-    //this.model.addListener(newUser -> this.UpdateUsersList(newUser.toString,true), UserLite.class, ModelUpdateTypes.NEW_USER);
-
-    // If the model triggers an "DELETE_USER" event, call the "updateAllUsers" method
-    //this.model.addListener(newUser -> this.UpdateUsersList(newUser.toString,false), UserLite.class, ModelUpdateTypes.DELETE_USER);
-
-    private void UpdateUsersList(String user, boolean IsAddUser) {
-        this.usersList.getItems().clear();
-        //this.model.others.forEach(user -> this.usersList.getItems().add(user.getName()));
-
-        if (IsAddUser) {
-            usernamesList.add(user);
-        } else {
-            usernamesList.remove(user);
-        }
-
-        for (String element : usernamesList) {
-            this.usersList.getItems().add(element);
-        }
-    }
-
-    @FXML
-    private void handleSearch() {
-        ObservableList<String> filteredList = usernamesList.filtered(item -> item.toLowerCase().contains(searchField.getText().toLowerCase()));
-
-        this.usersList.getItems().clear();
-        // Mettre à jour la liste affichée dans la vue
-        for (String element : filteredList) {
-            this.usersList.getItems().add(element);
-        }
-
-    }
-
-
     public void initialize() {
 
-        //usersConnected = dataUserServices.getConnectedUsers();
-/*        System.out.println("coucou");
-        for (UserLite element : usersConnected) {
-            this.usersList.getItems().add(element.getUsername());
-        }*/
+        // Initializing the user list.
+        this.initializeUserList();
+    }
+
+    private void initializeUserList() {
 
         DataServicesProvider dataServicesProvider = this.controller.getDataServicesProvider();
         DataUserServices dataUserServices = dataServicesProvider.getDataUserServices();
 
-        // Adding a listener to get the new user connected
-       dataUserServices.addListener(user -> {
-            Platform.runLater(() -> {
-                try {
-                    this.usersList.getItems().add(user.getUsername());
+        // Refresh the user list with the connected users.
+        this.refreshUsersList();
 
-                } catch (Exception exception) {
-                    throw new RuntimeException(exception);
-                }
+        // Adding a listener to get the new user connected and to add it to the list.
+        dataUserServices.addListener(user -> {
+            Platform.runLater(() -> {
+                this.usersList.getItems().add(user.getUsername());
             });
         }, UserLite.class, ModelUpdateTypes.NEW_USER);
 
-        // Adding a listener to get the new user connected
+        // Adding a listener to get the user disconnected and remove it from the list.
         dataUserServices.addListener(user -> {
             Platform.runLater(() -> {
-                try {
-                    this.usersList.getItems().remove(user.getUsername());
-                } catch (Exception exception) {
-                    throw new RuntimeException(exception);
-                }
+                this.usersList.getItems().remove(user.getUsername());
             });
         }, UserLite.class, ModelUpdateTypes.DELETE_USER);
+    }
 
+    private void refreshUsersList() {
 
+        DataServicesProvider dataServicesProvider = this.controller.getDataServicesProvider();
+        DataUserServices dataUserServices = dataServicesProvider.getDataUserServices();
+
+        Map<UserLite, List<TrackLite>> connectedUsers = dataUserServices.getConnectedUsers();
+        Collection<UserLite> users = connectedUsers.keySet();
+
+        ObservableList<String> items = this.usersList.getItems();
+        items.clear();
+
+        users.forEach(user -> items.add(user.getUsername()));
+    }
+
+    @FXML
+    private void handleSearch() {
+
+        DataServicesProvider dataServicesProvider = this.controller.getDataServicesProvider();
+        DataUserServices dataUserServices = dataServicesProvider.getDataUserServices();
+
+        // Filtering the list using the user input.
+        String text = this.searchField.getText().toLowerCase();
+
+        // Refreshing the whole list.
+        if (text.isEmpty()) {
+            this.refreshUsersList();
+            return;
+        }
+
+        // Updating the list.
+        Map<UserLite, List<TrackLite>> users = dataUserServices.getConnectedUsers();
+
+        ObservableList<String> items = this.usersList.getItems();
+        items.clear();
+
+        users.keySet().stream()
+                .filter(user -> user.getUsername().toLowerCase().contains(text))
+                .forEach(user -> items.add(user.getUsername()));
     }
 }
