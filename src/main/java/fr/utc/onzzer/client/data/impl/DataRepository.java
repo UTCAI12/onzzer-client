5aa64ab8974ddad7d63d320ffef2c5191b1961dc
malorrx <malo.rrx@gmail.com>
2023-12-03 19:41:56 +0100

    implémentation Data-Tracks -1

--- src/main/java/fr/utc/onzzer/client/data/impl/DataRepository.java
+++ src/main/java/fr/utc/onzzer/client/data/impl/DataRepository.java
@@ -1,5 +1,6 @@
 package fr.utc.onzzer.client.data.impl;
 
+import fr.utc.onzzer.common.dataclass.Track;
 import fr.utc.onzzer.common.dataclass.User;
 import fr.utc.onzzer.common.dataclass.UserLite;
 import fr.utc.onzzer.common.dataclass.TrackLite;
@@ -9,21 +10,24 @@ import java.util.*;
 
 public class DataRepository {
 
-    public ArrayList<TrackLite> trackLites = new ArrayList<>();
+
+    public ArrayList<Track> tracks = new ArrayList<Track>();
+
+    public ArrayList<Track> downloadedTracks = new ArrayList<Track>();
 
     public HashMap<UserLite, List<TrackLite>> connectedUsers = new HashMap<>();
 
     public User user;
 
 
-    TrackLite getTrackByID(UUID trackId){
+    Track getTrackByID(UUID trackId){
         int track = 0;
-        for (int i =0; i<trackLites.size();i++){
-            if(trackLites.get(i).getId()==trackId){
+        for (int i =0; i<tracks.size();i++){
+            if(tracks.get(i).getId()==trackId){
                 track = i;
             }
         }
-        return this.trackLites.get(track);
+        return this.tracks.get(track);
     }
 
     HashMap<UserLite, List<TrackLite>> getConnectedUsers() {
