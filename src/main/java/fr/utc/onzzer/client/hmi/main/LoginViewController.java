a273a56d069fb0a12877691f2d5601d58a522821
Syr0ws <45719267+Syr0ws@users.noreply.github.com>
2023-12-07 17:22:07 +0100

    Adding code for profile import. (#59)

--- src/main/java/fr/utc/onzzer/client/hmi/main/LoginViewController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/main/LoginViewController.java
@@ -18,8 +18,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -44,7 +46,16 @@ public class LoginViewController {
     private VBox inputGroupPassword;
 
     @FXML
+    private TextField pseudoInput;
+
+    @FXML
+    private TextField passwordInput;
+
+    @FXML
     private Label loginError;
+
+    @FXML
+    private Label importError;
 
     @FXML
     private void onRegisterLabelClick() throws IOException {
@@ -188,6 +199,45 @@ public class LoginViewController {
         }
     }
 
+    @FXML
+    private void onImportButtonClick() {
+
+        // Hide previous errors.
+        this.hideImportError();
+
+        DataServicesProvider dataServicesProvider = this.controller.getDataServicesProvider();
+        DataUserServices userServices = dataServicesProvider.getDataUserServices();
+
+        // File selection.
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choisir le fichier de profil.");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Profiles", "*.ser"));
+
+        File selectedFile = fileChooser.showOpenDialog(MainClient.getStage());
+
+        if(selectedFile == null) {
+            return;
+        }
+
+        // Importing profile.
+        String filePath = selectedFile.getAbsolutePath();
+
+        try{
+
+            User user = userServices.importProfile(filePath);
+
+            this.pseudoInput.setText(user.getUsername());
+            this.passwordInput.setText(user.getPassword());
+
+        } catch (Exception exception){
+
+            exception.printStackTrace();
+
+            this.showImportError();
+        }
+    }
+
     private void login(DataUserServices userServices, ComMainServices comServices) throws Exception {
 
         // Preparing data to send.
@@ -239,5 +289,15 @@ public class LoginViewController {
     private void hideGlobalError() {
         this.loginError.setVisible(false);
         this.loginError.setManaged(false);
+    }
+
+    private void showImportError() {
+        this.importError.setVisible(true);
+        this.importError.setManaged(true);
+    }
+
+    private void hideImportError() {
+        this.importError.setVisible(false);
+        this.importError.setManaged(false);
     }
 }
