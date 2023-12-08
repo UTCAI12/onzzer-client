8decfebbadd52e64eaa2a0726f75addef7bf9b14
MyriemKh <myriem.khal@gmail.com>
2023-12-08 11:47:14 +0100

    change fxml and controller listen

--- src/main/java/fr/utc/onzzer/client/hmi/music/ListenTrackViewController.java
+++ src/main/java/fr/utc/onzzer/client/hmi/music/ListenTrackViewController.java
@@ -8,6 +8,8 @@ import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Text;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.io.IOException;
@@ -74,33 +76,39 @@ public class ListenTrackViewController {
         } else {
             loadTrackAtIndex(trackArrayList.size() - 1);
         }
-
-        this.onClickPlayPause();
     }
 
     @FXML
     private void onClickPlayPause() {
-
-        if (this.mediaPlayer != null) {
-            this.mediaPlayer.stop();
-        }
-
         final File file;
         try {
+            System.out.println(this.mediaPlayer);
             file = new File(this.track.asMp3File(this.track.getTitle()));
-            System.out.println(file);
 
             if (this.track.getTitle() != null) this.txtTitle.setText(this.track.getTitle());
             if (this.track.getAuthor() != null) this.txtAuthor.setText(this.track.getAuthor());
             if (this.track.getAlbum() != null) this.txtAlbum.setText(this.track.getAlbum());
+
             this.sound = new Media(file.toURI().toString());
-            this.mediaPlayer = new MediaPlayer(sound);
 
             System.out.println(mediaPlayer);
-
+            if (this.mediaPlayer == null) {
+                this.mediaPlayer = new MediaPlayer(sound);
+                this.mediaPlayer.setOnEndOfMedia(() -> {
+                    onClickNextTrack();
+                });
+                this.mediaPlayer.play();
+            } else {
+                if (this.mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED) {
+                    this.mediaPlayer.play();
+                } else if (this.mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+                    this.mediaPlayer.pause();
+                }
+            }
             this.mediaPlayer.currentTimeProperty().addListener((obs, oldValue, newValue) -> {
                 if (this.sound == null)
                     return;
+                System.out.println(newValue);
                 this.txtCurrentTime.setText((int) newValue.toSeconds() + "s");
                 this.txtTrackDuration.setText((int) this.sound.getDuration().toSeconds() + "s" );
                 this.sliderTrackDuration.setValue((int) ((newValue.toSeconds() / this.sound.getDuration().toSeconds()) * 100) );
@@ -118,12 +126,21 @@ public class ListenTrackViewController {
         } else {
             loadTrackAtIndex(0);
         }
+    }
 
-        this.onClickPlayPause();
+    @FXML
+    private void onSliderReleased(MouseEvent event) {
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            double seekTime = (sliderTrackDuration.getValue() / 100) * sound.getDuration().toSeconds();
+            mediaPlayer.seek(Duration.seconds(seekTime));
+        }
     }
 
     private void loadTrackAtIndex(int index) {
+        this.mediaPlayer.stop();
+        this.mediaPlayer = null;
         track = trackArrayList.get(index);
         trackIndex = index;
+        this.onClickPlayPause();
     }
 }
