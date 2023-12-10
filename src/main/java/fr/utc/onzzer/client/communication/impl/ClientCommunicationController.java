4118b1d39e65fba22a7d48133d02569002168883
Riyad A <146223728+RiyadA0@users.noreply.github.com>
2023-12-10 22:45:50 +0100

    imppl of disconect

--- src/main/java/fr/utc/onzzer/client/communication/impl/ClientCommunicationController.java
+++ src/main/java/fr/utc/onzzer/client/communication/impl/ClientCommunicationController.java
@@ -125,7 +125,7 @@ public class ClientCommunicationController implements ComMainServices, ComMusicS
 
     @Override
     public void disconnect() throws Exception {
-
+        this.sendServer(SocketMessagesTypes.USER_DISCONNECT, this.clientModel.getUser());
     }
 
     @Override
