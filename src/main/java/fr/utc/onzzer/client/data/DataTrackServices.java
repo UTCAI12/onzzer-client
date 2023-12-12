e225804d8ebccf27284ff01c96dcf1f7fa350e3c
matthewjoy-prog <73708506+matthewjoy-prog@users.noreply.github.com>
2023-12-12 10:48:55 +0100

    add extends Service and Listenable (#92)

--- src/main/java/fr/utc/onzzer/client/data/DataTrackServices.java
+++ src/main/java/fr/utc/onzzer/client/data/DataTrackServices.java
@@ -2,11 +2,12 @@ package fr.utc.onzzer.client.data;
 
 import fr.utc.onzzer.common.dataclass.TrackLite;
 import fr.utc.onzzer.common.dataclass.Track;
+import fr.utc.onzzer.common.services.Service;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
-public interface DataTrackServices {
+public interface DataTrackServices extends Service {
     void saveTrack(Track track) throws Exception;
     void updateTrack(Track track) throws Exception;
     Track getTrack(UUID uuid);
--- src/main/java/fr/utc/onzzer/client/data/impl/DataCommentServicesImpl.java
+++ src/main/java/fr/utc/onzzer/client/data/impl/DataCommentServicesImpl.java
@@ -2,10 +2,11 @@ package fr.utc.onzzer.client.data.impl;
 
 import fr.utc.onzzer.client.data.DataCommentServices;
 import fr.utc.onzzer.common.dataclass.Comment;
+import fr.utc.onzzer.common.services.Listenable;
 
 import java.util.UUID;
 
-public class DataCommentServicesImpl implements DataCommentServices {
+public class DataCommentServicesImpl extends Listenable implements DataCommentServices {
 
     public DataCommentServicesImpl() {}
     @Override
