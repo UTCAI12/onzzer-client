bbcc695c1d447c7700f6befce0eac9feb368f1f1
chevremaudite <nicomj27@gmail.com>
2023-12-13 18:40:57 +0100

    edit track wip

--- src/main/java/fr/utc/onzzer/client/hmi/music/services/ViewMusicServices.java
+++ src/main/java/fr/utc/onzzer/client/hmi/music/services/ViewMusicServices.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public interface ViewMusicServices {
     void openSearchTracks(Scene scene) throws IOException;
     void openCreateTrack() throws IOException;
-    void openEditTrack(UUID trackId) throws IOException;
+    void openEditTrack(UUID trackId) throws Exception;
     void openDeleteTrack(UUID trackId) throws IOException;
     void openMediaPlayer(Scene scene, UUID trackId) throws IOException;
 }
